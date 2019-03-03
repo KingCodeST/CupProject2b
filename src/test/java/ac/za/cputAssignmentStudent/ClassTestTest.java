e675f864b80b580fda05b6a6bef1767ff93011e5
Siphokuhle <cyfotyasi@gmail.com>
2019-03-03 03:58:05 +0200

    commetted all the class

---
+++ src/test/java/ac/za/cputAssignmentStudent/ClassTestTest.java
@@ -0,0 +1,28 @@
+package ac.za.cputAssignmentStudent;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ClassTestTest {
+
+
+
+    @Test
+    public void getMarks() {
+    }
+
+    @Test
+    public void getMarksPerTest() {
+        Assert.assertTrue("",true);
+    }
+
+    @Test
+    public void setMarksPerTest() {
+        Assert.assertTrue("",true);
+    }
+
+    @Test
+    public String toString() {
+        return null;
+    }
+}
