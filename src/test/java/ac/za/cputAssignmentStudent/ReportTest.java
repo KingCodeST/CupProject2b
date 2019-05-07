e6d113f0e979278ab1bab7c1278c7ffcdaabe604
Siphokuhle <cyfotyasi@gmail.com>
2019-05-07 03:21:10 +0200

    commetted all the class

--- src/test/java/ac/za/cputAssignmentStudent/ReportTest.java
+++ src/test/java/ac/za/cputAssignmentStudent/ReportTest.java
@@ -7,48 +7,40 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ReportTest {
-
+        Report report;
     @Before
     public void setUp() throws Exception {
+        report =new Report("T","3232323",56,6787);
     }
 
-    @After
-    public void tearDown() throws Exception {
-    }
 
     @Test
     public void getStudNu() {
+        assertNotNull("54r",report.getStudNu());
     }
 
-    @Test
-    public void setStudNu() {
-    }
 
     @Test
     public void getStudDetails() {
+        assertNotNull("Adp =99 Mark for the year ADT =88 ",report.getStudDetails());
     }
 
-    @Test
-    public void setStudDetails() {
-    }
 
     @Test
     public void getNumber() {
+        assertNotNull("56",report.getNumber());
     }
 
-    @Test
-    public void setNumber() {
-    }
+
 
     @Test
     public void getModuleFee() {
+        assertNotNull(String.valueOf(56),report.getModuleFee());
     }
 
-    @Test
-    public void setModuleFee() {
-    }
 
     @Test
     public void getMarks() {
+        assertNotNull(String.valueOf(100),report.getMarks());
     }
 }
